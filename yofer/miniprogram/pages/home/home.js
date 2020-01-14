52f32019ef0c2d93b3a876fa18b9f0a145d423ff
xiaofei <1178862608@qq.com>
2020-01-14 20:34:50 +0800

    home组件更新1.14

--- yofer/miniprogram/pages/home/home.js
+++ yofer/miniprogram/pages/home/home.js
@@ -5,62 +5,78 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+		swiper:[
+			{
+				title: "双十一活动",
+				subTitle: "双十一到了, 不给您的爱车来一件新衣服,换双新鞋吗?",
+				src:"../../images/swiper1.png"
+			},
+			{
+				title: "春节放假通知",
+				subTitle: "Yofer于1月14日进入春节假期, 祝大家新年快乐!",
+				src: "../../images/swiper2.png"
+			},
+			{
+				title: "宝马三系套件",
+				subTitle: "宝马三系上市, 一改市面上现有仿M3套件的千篇一律,让您的三系拥有全新外观!",
+				src: "../../images/swiper4.png"
+			}
+		]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
